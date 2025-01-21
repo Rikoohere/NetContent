a3ea15ff77608964766e06b0870aa4349d8e662d
Rikoohere <ahmedabukhurma9@gmail.com>
2025-01-21 09:28:14 +0300

    Captcha Fix

--- netlify/functions/captcha.js
+++ netlify/functions/captcha.js
@@ -1,7 +1,6 @@
 const svgCaptcha = require('svg-captcha');
 
 exports.handler = async (event, context) => {
-  // Check if the HTTP method is GET
   if (event.httpMethod !== 'GET') {
     return {
       statusCode: 405,
@@ -12,10 +11,16 @@ exports.handler = async (event, context) => {
   // Generate a new CAPTCHA
   const captcha = svgCaptcha.create();
 
-  // Return the CAPTCHA image as an SVG
+  // Set a cookie with the CAPTCHA answer
+  const cookie = `captchaAnswer=${captcha.text}; Path=/; HttpOnly; SameSite=Strict; Max-Age=300`; // Expires in 5 minutes
+
+  // Return the CAPTCHA image as an SVG with the cookie
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'image/svg+xml' },
+    headers: {
+      'Content-Type': 'image/svg+xml',
+      'Set-Cookie': cookie,
+    },
     body: captcha.data,
   };
 };
--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,31 +1,30 @@
-const svgCaptcha = require('svg-captcha');
-
-let currentCaptchaAnswer = '';
-let userHearts = 3;
-
 exports.handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: 'Method Not Allowed' }),
-    };
-  }
-
-  const body = JSON.parse(event.body);
-  const userAnswer = body.answer;
-
-  if (userAnswer === currentCaptchaAnswer) {
-    const captcha = svgCaptcha.create();
-    currentCaptchaAnswer = captcha.text;
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true, captcha: captcha.data }),
-    };
-  } else {
-    userHearts -= 1;
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: false, hearts: userHearts }),
-    };
-  }
-};
+    if (event.httpMethod !== 'POST') {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ message: 'Method Not Allowed' }),
+      };
+    }
+  
+    // Parse the CAPTCHA answer from the cookie
+    const cookies = event.headers.cookie || '';
+    const captchaAnswer = cookies
+      .split(';')
+      .find(c => c.trim().startsWith('captchaAnswer='))
+      ?.split('=')[1];
+  
+    const body = JSON.parse(event.body);
+    const userAnswer = body.answer;
+  
+    if (userAnswer === captchaAnswer) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true }),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: false }),
+      };
+    }
+  };
